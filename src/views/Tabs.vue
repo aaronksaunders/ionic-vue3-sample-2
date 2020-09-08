ac6e3af690dbee6c42c4306e6a0fd1abf76c8d4c
Aaron Saunders <aaronksaunders@gmail.com>
2020-09-08 14:23:52 -0400

    fix icons

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -4,17 +4,17 @@
       <ion-tabs>
         <ion-tab-bar slot="bottom">
           <ion-tab-button tab="list" href="/tabs/list">
-            <ion-icon :icon="homeOutline" />
+            <ion-icon :icon="imageOutline" />
             <ion-label>List All</ion-label>
           </ion-tab-button>
-            
+
           <ion-tab-button tab="upload" href="/tabs/upload">
-            <ion-icon :icon="settingsOutline" />
+            <ion-icon :icon="cloudOutline" />
             <ion-label>Upload</ion-label>
           </ion-tab-button>
-          
+
           <ion-tab-button tab="geolocation" href="/tabs/geolocation">
-            <ion-icon :icon="square" />
+            <ion-icon :icon="locateOutline" />
             <ion-label>Location</ion-label>
           </ion-tab-button>
         </ion-tab-bar>
@@ -24,18 +24,34 @@
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonContent, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import {  square, settingsOutline, homeOutline } from 'ionicons/icons';
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonContent,
+  IonLabel,
+  IonIcon,
+  IonPage,
+} from "@ionic/vue";
+import { cloudOutline, imageOutline, locateOutline } from "ionicons/icons";
 
 export default {
-  name: 'Tabs',
-  components: { IonContent, IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  name: "Tabs",
+  components: {
+    IonContent,
+    IonLabel,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonPage,
+  },
   setup() {
     return {
-      homeOutline, 
-      square, 
-      settingsOutline,
-    }
-  }
-}
+      cloudOutline,
+      imageOutline,
+      locateOutline,
+    };
+  },
+};
 </script>
